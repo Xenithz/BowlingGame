300a3b2089671fbe372b2c56f4030eb6ba5e6f95
Xenithz <thatpersonovathere@gmail.com>
2018-08-22 01:46:06 +0400

    Project folder placed

---
+++ BowlingPrototype/Assets/Scripts/SwipeBowlingBall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeBowlingBall : MonoBehaviour 
+{
+	public Vector3 startingPosition;
+
+    private void Update()
+    {
+        
+    }
+}
